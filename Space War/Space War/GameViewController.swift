ab677d73ff171e4deb5636366dd8b00d5a2741c3
onebroccoli <three.broccoli@gmail.com>
2021-10-04 17:05:16 -0700

    Update GameViewController.swift

--- Space War/Space War/GameViewController.swift
+++ Space War/Space War/GameViewController.swift
@@ -8,13 +8,32 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameViewController: UIViewController {
 
+    var backingAudio = AVAudioPlayer()
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+        let filePath = Bundle.main.path(forResource: "bensound-epic", ofType: "mp3")
+        let audioNSURL = NSURL(fileURLWithPath: filePath!)
+        
+        do {
+            backingAudio = try AVAudioPlayer(contentsOf: audioNSURL as URL)
+            
+        }
+        catch {
+            return print("cannot find the audio")
+        }
+        
+        backingAudio.numberOfLoops = -1
+//        backingAudio.volume = 1
+        backingAudio.play()
+        
+        let scene = MainMenuScene(size: CGSize(width: 1536, height: 2048))
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
